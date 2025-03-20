33ff4c9628ef5889d61b997293c4ab3a4c54dee1
noobprofesor <noobprofesor@gmail.com>
2025-03-20 13:22:28 +0300

    refactor

--- src/main/java/com/example/systemstest/authentication/controller/AdminController.java
+++ src/main/java/com/example/systemstest/authentication/controller/AdminController.java
@@ -1,25 +1,17 @@
 
 package  com.example.systemstest.authentication.controller;
 
-import com.example.systemstest.authentication.model.ERole;
-import com.example.systemstest.authentication.model.Role;
 import com.example.systemstest.authentication.model.User;
 import com.example.systemstest.authentication.payload.repository.RoleRepository;
 import com.example.systemstest.authentication.payload.repository.UserRepository;
-import com.example.systemstest.authentication.payload.request.SignupRequest;
 import com.example.systemstest.authentication.payload.response.MessageResponse;
-import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @CrossOrigin(origins = "http://localhost:4200/", maxAge = 600, allowCredentials = "true")
 @RestController
